1bde30dcead0cbebaf4faa9c30789662e17b727c
nadiabsouza <nadialuna.souza@gmail.com>
2022-07-02 22:53:39 -0300

    first commit

---
+++ src/main/java/br/com/empregaelas/EmpregaelasApplication.java
@@ -0,0 +1,13 @@
+package br.com.empregaelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpregaelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpregaelasApplication.class, args);
+	}
+
+}
