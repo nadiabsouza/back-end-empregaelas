8654a495e30f1ac51e11334d519c7a963e9d50ca
Edlane Santana <92332100+edlanelima@users.noreply.github.com>
2022-08-02 18:23:45 -0300

    Update V7_Create_Candidatura.sql

--- src/main/resources/db/migration/V7_Create_Candidatura.sql
+++ src/main/resources/db/migration/V7_Create_Candidatura.sql
@@ -3,7 +3,7 @@ CREATE TABLE `ee_candidatura` (
 `id_usuario` bigint NOT NULL,
 `id_vaga` bigint NOT NULL,
 PRIMARY KEY (`id`),
-FOREIGN KEY (`id_usuario`) REFERENCES ee_usuario(id)
-FOREIGN KEY (`id_vaga`) REFERENCES ee_vaga(id)
-);
+FOREIGN KEY (`id_vaga`) REFERENCES ee_vaga(id),
+FOREIGN KEY (`id_candidato`) REFERENCES ee_candidato(id)
+); 
 
