6733a1b21310f52235d11f657788134a34ae38c7
Marisa Marzanasco <mmarzanweb@gmail.com>
2022-08-13 23:19:00 -0300

    criação de componentes e rotas

--- front_angular/src/app/app.module.ts
+++ front_angular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CadastroComponent } from './components/pages/cadastro/cadastro.component';
+import { CandidatasComponent } from './components/pages/candidatas/candidatas.component';
+import { EmpresasComponent } from './components/pages/empresas/empresas.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { InstitucionalComponent } from './components/pages/institucional/institucional.component';
+import { VagasComponent } from './components/pages/vagas/vagas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CadastroComponent,
+    CandidatasComponent,
+    EmpresasComponent,
+    HomeComponent,
+    InstitucionalComponent,
+    VagasComponent
   ],
   imports: [
     BrowserModule,
