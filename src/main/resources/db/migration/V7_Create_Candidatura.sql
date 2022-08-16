0be1629ceeec39d3a19c131cb97124236bba9cec
Marisa Marzanasco <42682790+mmarzanweb@users.noreply.github.com>
2022-08-15 22:33:47 -0300

    Delete src/main directory

--- src/main/resources/db/migration/V7_Create_Candidatura.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `ee_candidatura` (
-`id` bigint NOT NULL AUTO_INCREMENT,
-`id_usuario` bigint NOT NULL,
-`id_vaga` bigint NOT NULL,
-PRIMARY KEY (`id`),
-FOREIGN KEY (`id_usuario`) REFERENCES ee_usuario(id)
-FOREIGN KEY (`id_vaga`) REFERENCES ee_vaga(id)
-);
-
